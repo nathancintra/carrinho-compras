338265962f023bc4f01ac30c98b257c198b2c766
Nathan Cintra <nathancintra@gmai.com>
2025-02-17 20:28:58 -0300

    Inicio

---
+++ js/app.js
@@ -0,0 +1,9 @@
+function adicionar(){
+    //recupera valor, quantidade e nome
+    let nomeProd = document.getElementById('produto');
+    let quantidade = document.getElementById('quantidade');
+}
+
+function limpar(){
+
+}
