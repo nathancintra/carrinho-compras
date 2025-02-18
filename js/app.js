4a18b0ecbaf67c3586ef2e9964583445f4f211eb
Nathan Cintra <nathancintra@gmai.com>
2025-02-17 21:34:03 -0300

    Adição de elementos ao carrinho e calculo total do carrinho.

--- js/app.js
+++ js/app.js
@@ -1,18 +1,29 @@
+let totalCarrinho = 0;
+limpar();
+
 function adicionar(){
     //recupera valor, quantidade e nome
     let produto = document.getElementById('produto').value;
     let nomeProduto = produto.split('-')[0];
     let valorUnitario = produto.split('$')[1];
-    let quantidade = document.getElementById('quantidade');
+    let quantidade = document.getElementById('quantidade').value;
     //calculo do subtotal
-    let preco = parseInt(quantidade.value * valorUnitario);
+    let preco = parseInt(quantidade * valorUnitario);
     //adicionar ao carrinho
     let carrinho = document.getElementById('lista-produtos');
     carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
-          <span class="texto-azul">${quantidade.value}</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
+          <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
         </section>`
+    //calculo do total do carrinho
+    totalCarrinho = totalCarrinho + preco;
+    let campoTotal = document.getElementById('valor-total');
+    campoTotal.textContent = `R$ ${totalCarrinho}`;
+    document.getElementById('quantidade').value = 0;
 }
 
 function limpar(){
+    totalCarrinho = 0;
+    document.getElementById('lista-produtos').innerHTML = '';
+    document.getElementById('valor-total').textContent = 'R$ 0';
 
 }
